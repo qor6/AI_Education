1e1c2ee02932c2ff74f35426fd7c05b4a55e75a2
BaekSumin <87318054+qor6@users.noreply.github.com>
2023-07-06 10:29:14 +0900

    chat-gpt test.py

--- knowledgeGraph/test.py
+++ knowledgeGraph/test.py
@@ -6,6 +6,6 @@ import openai
 # Load your API key from an environment variable or secret management service
 openai.api_key = os.getenv("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")
 
-#https://platform.openai.com/docs/libraries
+#링크 : https://platform.openai.com/docs/libraries
 response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[{"role": "user", "content": "Hello world"}])
 print(response)
